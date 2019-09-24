6e1335bb5192cb2985883dbcbe93dbe31732ccee
Robert Muthuri <muthuri.r@gmail.com>
2019-09-24 17:49:13 +0300

    Adds Ranger class.

---
+++ src/main/java/Ranger.java
@@ -0,0 +1,12 @@
+public class Ranger {
+
+    String name;
+    private int id;
+
+    public Ranger(String name) {
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+}

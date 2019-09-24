088eadf22ec8b7a0105dafed5330ab762e106754
Robert Muthuri <muthuri.r@gmail.com>
2019-09-24 22:15:37 +0300

    Adds a test class for Sighting class, a test and method to instantiate objects correctly.

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,4 +1,17 @@
+import org.sql2o.*;
+import java.util.List;
+import java.util.Objects;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class Sighting {
+    private int id;
+    private int animal_id;
+    private int ranger_id;
+    private int location_id;
+    private int sightedAt;
+
     public Sighting() {
     }
 }
--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -1,5 +1,18 @@
 import static org.junit.Assert.*;
+import org.junit.*;
 
 public class SightingTest {
 
+    // Setup helper method
+    public Sighting setupSighting(){
+        return new Sighting();
+    }
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void sighting_instantiatesCorrectly_true() {
+        Sighting testSighting = setupSighting();
+        assertEquals(true, testSighting instanceof Sighting);
+    }
 }
